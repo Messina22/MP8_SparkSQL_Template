e8dcb1f4554bc104a54482c3038d411349b7e88d
Nick Messina <nicknmessina@gmail.com>
2024-04-03 17:12:21 -0500

    Completed Part F

--- python/MP8_PartF.py
+++ python/MP8_PartF.py
@@ -18,11 +18,34 @@ spark = SparkSession.builder.getOrCreate()
 # Columns:
 # 0: word (string), 1: year (int), 2: frequency (int), 3: books (int)
 
+f = sc.textFile("gbooks")
+data_rdd = f.map(
+    lambda line: (line.split()[0], int(line.split()[1]), int(line.split()[2]))
+)
+
+schema = StructType(
+    [
+        StructField("word", StringType(), True),
+        StructField("year", IntegerType(), True),
+        StructField("frequency", IntegerType(), True),
+    ]
+)
+
+df = data_rdd.toDF(schema)
 
 ###
 # 2. Frequency Increase : analyze the frequency increase of words starting from the year 1500 to the year 2000
 ###
 # Spark SQL - DataFrame API
+
+new_df = df.filter(df.year >= 1500).filter(df.year <= 2000)
+
+window = Window.partitionBy("word").orderBy("year")
+
+df_lag = new_df.withColumn("lag", lag("frequency", -1).over(window))
+df_word_increase = df_lag.groupBy("word").agg(sum("lag").alias("total_increase"))
+
+df_word_increase.orderBy(col("total_increase").desc()).show()
 
 
 # df_word_increase.show()
